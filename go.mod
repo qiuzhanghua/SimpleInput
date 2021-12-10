c4fb59fbdf8fc4db4994d0fb293ce941fd493022
qiuzhanghua <qiuzhanghua@icloud.com>
2021-12-10 08:15:59 +0800

    test term again

--- go.mod
+++ go.mod
@@ -2,10 +2,6 @@ module SimpleInput
 
 go 1.17
 
-require github.com/qiuzhanghua/go-input v0.1.1
+require golang.org/x/term v0.0.0-20210927222741-03fcf44c2211
 
-require (
-	golang.org/x/crypto v0.0.0-20211202192323-5770296d904e // indirect
-	golang.org/x/sys v0.0.0-20211205182925-97ca703d548d // indirect
-	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
-)
+require golang.org/x/sys v0.0.0-20211205182925-97ca703d548d // indirect
--- main.go
+++ main.go
@@ -2,32 +2,52 @@ package main
 
 import (
 	"fmt"
-	"github.com/qiuzhanghua/go-input"
+	"golang.org/x/term"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 func main() {
-
-	ui := &input.UI{
-		Writer: os.Stdout,
-		Reader: os.Stdin,
+	termState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Failed to set raw mode on STDIN: %v\n",
+			err)
+		return
 	}
+	n := term.NewTerminal(os.Stdin, "tdp> ")
+	_ = n.SetSize(int(^uint(0)>>1), 0)
+	var ln, input string
 
-	query := "What is your name?"
-	name, err := ui.Ask(query, &input.Options{
-		//Default:  "Daniel",
-		Required: true,
-		Loop:     true,
-	})
-
-	fmt.Println(name, err)
-
-	query = "Which language do you prefer to use?"
-	lang, err := ui.Select(query, []string{"go", "Go", "golang"}, &input.Options{
-		Default: "Go",
-		Loop:    true,
-	})
-
-	fmt.Println(lang, err)
-
+	for {
+		ln, err = n.ReadLine()
+		_ = term.Restore(int(os.Stdin.Fd()), termState)
+		if err != nil {
+			break
+		}
+		input = strings.Trim(ln, "\n ")
+		switch {
+		case input == "exit":
+			os.Exit(0)
+		case input == "pwd":
+			cmd := exec.Command("pwd")
+			//cmd := exec.Command("gradle", "build")
+			cmd.Stdout = os.Stdout
+			cmd.Env = append([]string{"UI=path_to_tdp_ui"}, os.Environ()...)
+			// fmt.Println(cmd.Env)
+			if err := cmd.Run(); err != nil {
+				_, _ = fmt.Fprintf(os.Stderr, "Failed: %v\n",
+					err)
+			}
+			// Add more
+		default:
+			fmt.Println(input)
+		}
+		termState, err = term.MakeRaw(int(os.Stdin.Fd()))
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Failed to set raw mode on STDIN: %v\n",
+				err)
+			break
+		}
+	}
 }

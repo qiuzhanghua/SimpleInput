6aa11d9356efcae66b1cbd6e90bb60bd32660326
qiuzhanghua <qiuzhanghua@icloud.com>
2021-12-11 08:40:33 +0800

    back to go-input

--- go.mod
+++ go.mod
@@ -2,9 +2,10 @@ module SimpleInput
 
 go 1.17
 
-require (
-	github.com/magiconair/properties v1.8.5
-	github.com/qiuzhanghua/i10n v0.2.0
-)
+require github.com/qiuzhanghua/go-input v0.2.0
 
-require golang.org/x/text v0.3.7 // indirect
+require (
+	golang.org/x/crypto v0.0.0-20211202192323-5770296d904e // indirect
+	golang.org/x/sys v0.0.0-20211205182925-97ca703d548d // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+)
--- main.go
+++ main.go
@@ -2,29 +2,17 @@ package main
 
 import (
 	"fmt"
-	"github.com/magiconair/properties"
-	"github.com/qiuzhanghua/i10n"
-	"io/ioutil"
-	"log"
-	"os"
-	"path/filepath"
+	"github.com/qiuzhanghua/go-input"
 )
 
 func main() {
-	filename := "data" + string(filepath.Separator) + "app_zh.properties"
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-	data, err := ioutil.ReadAll(file)
-	if err != nil {
-		log.Fatal(err)
-	}
-	p, _ := properties.Load(data, properties.UTF8)
-	tag := i10n.ParseTagWithDefault(filename)
-	i10n.SetDefaultLang("zh-CN")
-	i10n.AddTagMap(tag, p.Map())
-	fmt.Println(i10n.TT("hello", tag))
-	fmt.Println(i10n.T("hello", "Daniel"))
+	ui := input.DefaultUI()
+
+	query := "What is your name?"
+	name, err := ui.Ask(query, &input.Options{
+		//		Default:  "qiuzhanghua",
+		Required: true,
+		Loop:     true,
+	})
+	fmt.Println(name, err)
 }
